18b47857708e6029cdabc08e997c2e907a2af3ad
Miroslav Rovis <miro.rovis@croatiafidelis.hr>
2016-02-06 06:11:51 +0100

    Cleaned some of the explanatory text, pre-0.18

--- tshark-streams.sh
+++ tshark-streams.sh
@@ -12,22 +12,41 @@
 # you know Matt, tell him to try and contact me, via github, probably most
 # reliable. 
 #
-# If option "-Y $DISPLAYFILTER" is *not* given, this script will extract all
-# tcp/ssl streams from your pcap file.
-#
 # Apart from a recent Wireshark install, xxd (part of vim-core here) is needed.
 #
-# Surely for ssl streams, the $SSLKEYLOGFILE must have been set for those ole
-# captures, but learn that howto elsewhere (the fact of the matter is, also
-# from links in the links below --not in first links-- you can).
+# If neither of the options "-Y $DISPLAYFILTER" or -l "$STREAMSLIST" is given,
+# but only the -r "$PCAP_FILE" (and -k "$KEYLOGFILE" if there are SSL streams
+# in the $PCAP_FILE), this script will extract all tcp(/ssl) streams from your
+# pcap file.
 #
-# There is a Gentoo Forums topic:
+# For the basics of SSL decryption see:
+# https://wiki.wireshark.org/SSL
 #
+# As far as decrypting your own captures (setting the $SSLKEYLOGFILE
+# environment variable etc.), you may try and see how I do it at:
+# https://github.com/miroR/uncenz
+#
+# Less important links follow (but if any of the links died by the time you
+# read here, do try and tell me --use the uncenz above to be able to prove you
+# tried to contact me by posting the screencast and traffic dump in public (the
+# only way to fight censorship; in Croatia we are still not done at all with
+# the remnants and progenie and metamorphoids of the communist UDBA; UDBA is
+# something like a tiny NSA, for a short description; and emails/phone
+# calls/other to me/from me have for years been filtered by those subjects and
+# allowed through or disallowed and cut out instead)--, and I'll try and post,
+# the
+# info/SSL-extracted-from-my-archives-same-information-as-previously-was-in-that-location,
+# on http://www.CroatiaFidelis.hr):
+#
+# My first acquainting myself with SSL decryption was at:
+# SSL Decode & My Hard-Earned Advice for SPDY/HTTP2 in Firefox
+# https://forums.gentoo.org/viewtopic-t-1029408.html
+#
+# How I started this script is all in this Gentoo Forums topic:
 # How to extract content from tshark-saved streams?
 # https://forums.gentoo.org/viewtopic-t-1033844.html
 #
-# and a wireshark-users mailing list lonely thread:
-#
+# and in a wireshark-users mailing list lonely thread:
 # [Wireshark-users] follow [tcp|ssl].stream with tshark 
 # https://www.wireshark.org/lists/wireshark-users/201511/msg00033.html 
 #
@@ -35,13 +54,39 @@
 # Bugzilla about this! But find those in the links above, if you really wish.)
 #
 # For the understanding of that topic and that thread I leave the old
-# tshark-streams.sh script in its original directory, where this complete
-# script is too. However, that old script, in case you got here via those, is
-# now named:
+# tshark-streams.sh script in its original directory, where the first improved
+# version of this script is too:
+#
+# LINK HERE
+#
+# However, neither those two old scripts, in case you got here via those, not
+# the initial one, that is now named:
 #
 # tshark-streams-INCOMPLETE.sh
 #
-# and should not be used at all.
+# and not either the first improved version, should be used anymore at all.
+#
+# On the other hand, I work so slowly, that I'm currently out of time to clean
+# up this script (and my head already aches from strain to figure out how to do
+# it), which I intend to tag version 0.18 of tshark-streams.sh ...
+#
+# Ah, I've only managed to get it to work with Bash's own getopts, after
+# pondering over NetMinecraft (thanks Jonathan Racicot!):
+#
+# LINK HERE
+#
+# (but NetMinecraft was done for the pre-2.0 Wireshark it seems to me, and some of
+# the functionality does not seem to work, or is incomplete)
+#
+# Use this script absolutely at your own risk! ...However, it works for me (mostly).
+#
+# I guarrantee nothing to you regarding anything at all, usefulness or goodness
+# or anything in/from/connected to this script.
+#
+# Released under BSD license, pls. see LICENSE, attached to this script (if
+# not, it's under a generic BSD GNU-compatible license)
+#
+# Copyright (c) 2016 Croatia Fidelis, Miroslav Rovis, www.CroatiaFidelis.hr
 #
 
 function show_help {
@@ -104,7 +149,6 @@ fi
 echo \$KEYLOGFILE: $KEYLOGFILE
 read FAKE
 
-#
 echo -n \$PCAP_FILE: echo $PCAP_FILE
 read FAKE
 dump=$(echo $PCAP_FILE|cut -d. -f1)
@@ -117,14 +161,10 @@ filename=$dump.$ext
 echo -n \$filename: echo $filename
 read FAKE
 echo -n \$ext: echo $ext
-#if [ $ext != "pcap" ]; then
-#	ln -s $dump.$ext $dump.pcap
-#fi
-#dump=$(echo $1|sed 's/\.pcap//')
-#echo "\$dump.pcap: $dump.pcap"
 
 # I like to have a log to look up. Some PCAPs are slow to work. Need to know at
-# what stage the work is.
+# what stage the work is. (UPDATE: There is however the ${dump}_streams.ls-1 that gets created
+# at the start though, so maybe this tip is not needed.)
 # And I was finishing lots of line with "|& tee -a $tshlog". No. Clutters too much.
 # Better to add such a redirection to the very command issued when starting the script.
 # Something like the commented lines below (previously written for the further
@@ -143,30 +183,23 @@ if [ ! -z "$DISPLAYFILTER" ]; then
 	STREAMS=$(tshark -o "ssl.keylog_file: $KEYLOGFILE" -r "$dump.$ext" -Y "$DISPLAYFILTER" -T fields -e tcp.stream | sort -n | uniq)
 #	echo "\$STREAMS: $STREAMS"
 	echo $STREAMS | tr ' ' '\012' > ${dump}_streams.ls-1
+	echo "############################################################"
+	echo "( The list of stream numbers is in:"
+	ls -l ${dump}_streams.ls-1
+	echo "############################################################"
 	read FAKE
 
-if [ ! -z "$STREAMSLIST" ]; then
-	echo \$STREAMSLIST
-	read FAKE
-	echo \$STREAMSLIST: $STREAMSLIST
-	read FAKE
-	STREAMS=$(cat $STREAMSLIST)
-	echo \$STREAMS
-	read FAKE
-	echo "\$STREAMS: $STREAMS"
-	read FAKE
-fi
-#	elif [ ! -z "$3" ]; then
-#		echo "Trying to make $3 be an option to either:"
-#		echo "1) be the selection of streams to work instead of the entire array, or"
-#		echo "2) allow giving the selection of streams somehow (don't know how yet),"
-#		echo "after they are listed."
-#		echo "Only the 1) for now"
-#		echo "Give the text file with sole content the list of streams to process:"
-#		read selected_streams
-#		STREAMS=$(cat $selected_streams)
-#		else "This suboption not accounted for yet."
-#	fi
+	if [ ! -z "$STREAMSLIST" ]; then
+		echo \$STREAMSLIST
+		read FAKE
+		echo \$STREAMSLIST: $STREAMSLIST
+		read FAKE
+		STREAMS=$(cat $STREAMSLIST)
+		echo \$STREAMS
+		read FAKE
+		echo "\$STREAMS: $STREAMS"
+		read FAKE
+	fi
 else
 	echo "tshark -o "ssl.keylog_file: $KEYLOGFILE" -r $dump.$ext -T fields -e tcp.stream | sort -n | uniq"
 #	tshark -o "ssl.keylog_file: $KEYLOGFILE" -r "$dump.$ext" -T fields -e tcp.stream | sort -n | uniq
@@ -189,57 +222,56 @@ else
 	fi
 fi
 
-for i in $STREAMS
-do 
-		# I adjust this manually. If really huge dump, I set %.4d, else %.3d is enough
-		INDEX=`printf '%.3d' $i`
-		echo "Processing stream $INDEX ..."
+for i in $STREAMS; do 
+	# This can be adjusted manually. If really huge dump, I set %.4d, else %.3d is enough.
+	INDEX=`printf '%.3d' $i`
+	echo "Processing stream $INDEX ..."
 
-		tshark -o "ssl.keylog_file: $KEYLOGFILE" -r "$dump.$ext" -T fields -e data -qz follow,tcp,raw,$i | grep -E '[[:print:]]' > "${dump}"_s$INDEX.raw
+	tshark -o "ssl.keylog_file: $KEYLOGFILE" -r "$dump.$ext" -T fields -e data -qz follow,tcp,raw,$i | grep -E '[[:print:]]' > "${dump}"_s$INDEX.raw
 
-#		ls -l ${dump}_s$INDEX.raw
-		cat ${dump}_s$INDEX.raw \
-		| grep -A1000000000 =================================================================== \
-		> ${dump}_s$INDEX.raw.CLEAN ;
-		wc_l=$(cat ${dump}_s$INDEX.raw.CLEAN | wc -l) ; #echo $wc_l;
-		wc_l_head=$(echo $wc_l-1|bc); #echo $wc_l_head;
-		wc_l_tail=$(echo $wc_l_head-5|bc); #echo $wc_l_tail;
-		cat ${dump}_s$INDEX.raw.CLEAN | head -$wc_l_head|tail -$wc_l_tail > ${dump}_s$INDEX.raw.FINAL;
-#		ls -l ${dump}_s$INDEX.raw.CLEAN  ${dump}_s$INDEX.raw.FINAL;
-		cat ${dump}_s$INDEX.raw.FINAL | xxd -r -p > ${dump}_s$INDEX.bin
-		# To see why and if tshark still does in such way that this work, maybe sometime
-		# in the future, reverse the commenting of these two lines below in particular, and investigate
-		rm ${dump}_s$INDEX.raw*
-		echo "Extracted:"
-		ls -l ${dump}_s$INDEX.bin
+	ls -l ${dump}_s$INDEX.raw
+	cat ${dump}_s$INDEX.raw \
+	| grep -A1000000000 =================================================================== \
+	> ${dump}_s$INDEX.raw.CLEAN ;
+	wc_l=$(cat ${dump}_s$INDEX.raw.CLEAN | wc -l) ; #echo $wc_l;
+	wc_l_head=$(echo $wc_l-1|bc); #echo $wc_l_head;
+	wc_l_tail=$(echo $wc_l_head-5|bc); #echo $wc_l_tail;
+	cat ${dump}_s$INDEX.raw.CLEAN | head -$wc_l_head|tail -$wc_l_tail > ${dump}_s$INDEX.raw.FINAL;
+	ls -l ${dump}_s$INDEX.raw.CLEAN  ${dump}_s$INDEX.raw.FINAL;
+	cat ${dump}_s$INDEX.raw.FINAL | xxd -r -p > ${dump}_s$INDEX.bin
+	# To see why and if tshark still does in such way that this work, maybe sometime
+	# in the future, reverse the commenting of these two lines below in particular, and investigate
+	rm ${dump}_s$INDEX.raw*
+	echo "Extracted:"
+	ls -l ${dump}_s$INDEX.bin
 
-		tshark -o "ssl.keylog_file: $KEYLOGFILE" -r "$dump.$ext" -qz follow,tcp,ascii,$i | grep -E '[[:print:]]' > "${dump}"_s$INDEX.txt
-		echo "Extracted:"
-		ls -l ${dump}_s$INDEX.txt
+	tshark -o "ssl.keylog_file: $KEYLOGFILE" -r "$dump.$ext" -qz follow,tcp,ascii,$i | grep -E '[[:print:]]' > "${dump}"_s$INDEX.txt
+	echo "Extracted:"
+	ls -l ${dump}_s$INDEX.txt
 
-		tshark -o "ssl.keylog_file: $KEYLOGFILE" -r "$dump.$ext" -T fields -e data -qz follow,ssl,raw,$i | grep -E '[[:print:]]' > "${dump}"_s${INDEX}-ssl.raw
+	tshark -o "ssl.keylog_file: $KEYLOGFILE" -r "$dump.$ext" -T fields -e data -qz follow,ssl,raw,$i | grep -E '[[:print:]]' > "${dump}"_s${INDEX}-ssl.raw
 
-		cat ${dump}_s${INDEX}-ssl.raw \
-		| grep -A1000000000 =================================================================== \
-		> ${dump}_s${INDEX}-ssl.raw.CLEAN ;
-		wc_l=$(cat ${dump}_s${INDEX}-ssl.raw.CLEAN | wc -l) ; #echo $wc_l;
-		wc_l_head=$(echo $wc_l-1|bc); #echo $wc_l_head;
-		wc_l_tail=$(echo $wc_l_head-5|bc); #echo $wc_l_tail;
-		cat ${dump}_s${INDEX}-ssl.raw.CLEAN | head -$wc_l_head|tail -$wc_l_tail > ${dump}_s${INDEX}-ssl.raw.FINAL;
-#		ls -l ${dump}_s${INDEX}-ssl.raw.CLEAN  ${dump}_s${INDEX}-ssl.raw.FINAL;
-		cat ${dump}_s${INDEX}-ssl.raw.FINAL | xxd -r -p > ${dump}_s${INDEX}-ssl.bin
-		# To see why and if tshark still does in such way that this work, maybe sometime
-		# in the future, reverse the commenting of these two lines below in particular, and investigate
-		rm ${dump}_s${INDEX}-ssl.raw*
-		echo "Extracted:"
-		ls -l ${dump}_s$INDEX-ssl.bin
-		#read FAKE
+	cat ${dump}_s${INDEX}-ssl.raw \
+	| grep -A1000000000 =================================================================== \
+	> ${dump}_s${INDEX}-ssl.raw.CLEAN ;
+	wc_l=$(cat ${dump}_s${INDEX}-ssl.raw.CLEAN | wc -l) ; #echo $wc_l;
+	wc_l_head=$(echo $wc_l-1|bc); #echo $wc_l_head;
+	wc_l_tail=$(echo $wc_l_head-5|bc); #echo $wc_l_tail;
+	cat ${dump}_s${INDEX}-ssl.raw.CLEAN | head -$wc_l_head|tail -$wc_l_tail > ${dump}_s${INDEX}-ssl.raw.FINAL;
+	ls -l ${dump}_s${INDEX}-ssl.raw.CLEAN  ${dump}_s${INDEX}-ssl.raw.FINAL;
+	cat ${dump}_s${INDEX}-ssl.raw.FINAL | xxd -r -p > ${dump}_s${INDEX}-ssl.bin
+	# To see why and if tshark still does in such way that this work, maybe sometime
+	# in the future, reverse the commenting of these two lines below in particular, and investigate
+	rm ${dump}_s${INDEX}-ssl.raw*
+	echo "Extracted:"
+	ls -l ${dump}_s$INDEX-ssl.bin
+	#read FAKE
 
-		tshark -o "ssl.keylog_file: $KEYLOGFILE" -r "$dump.$ext" -qz follow,ssl,ascii,$i | grep -E '[[:print:]]' > "${dump}"_s${INDEX}-ssl.txt
-		echo "Extracted:"
-		ls -l ${dump}_s$INDEX-ssl.txt
-
+	tshark -o "ssl.keylog_file: $KEYLOGFILE" -r "$dump.$ext" -qz follow,ssl,ascii,$i | grep -E '[[:print:]]' > "${dump}"_s${INDEX}-ssl.txt
+	echo "Extracted:"
+	ls -l ${dump}_s$INDEX-ssl.txt
 done
 
-echo "The list of stream numbers is in:"
+echo "( The list of stream numbers is in:"
 ls -l ${dump}_streams.ls-1
+echo ")"
