e7f2c5ffc0e37b3f3ffd018c87a7d68184fef34b
Miroslav Rovis <miro.rovis@croatiafidelis.hr>
2026-04-15 16:55:23 +0200

    tls.streams list separately from tcp.streams
    each tls.stream was bind to tcp.stream in TLS1.2 and early TLS1.3
    Since late 2025 that started changing. tls.streams can go independently
    of tcp.streams, can start anywhere in a tcp.stream and can span into other
    tcp.streams
    So the script needed partial rewrite. Still dirty and possibly buggy.

--- tshark-streams.sh
+++ tshark-streams.sh
@@ -6,6 +6,7 @@
 #
 # Apart from a recent Wireshark/tshark install, xxd is needed.
 #
+#TO_DO update this text
 # If neither of the options "-Y $DISPLAYFILTER" or -l "$STREAMSLIST" is given,
 # but only the -r "$PCAP_FILE" (and -k "$KEYLOGFILE" if there are TLS streams
 # in the $PCAP_FILE), this script will extract all tcp(/tls) streams from your
@@ -30,6 +31,7 @@
 # Copyright (c) 2016,2021,2022,2023,2026 Croatia Fidelis, Miroslav Rovis, <https://www.CroatiaFidelis.hr>
 #
 
+#TO_DO update this text
 # Traces that, relative to available computing power, are too large, need to be
 # split to be worked. Today all is mostly TLS, so mostly only splitting by
 # streams allows for analysis. This script allows only for limiting traces to
@@ -47,6 +49,36 @@ function ask()
         y*|Y*) return 0 ;;
         *) return 1 ;;
     esac
+}
+
+function ls_head_tail () {
+    # we check $2 that it greps on any decimal number
+    if [ ! -e "$1" ]; then
+        echo "###########################################"
+        echo "$1 non extant (fix the script if wrong)"
+        sleep 5
+        echo "###########################################"
+    fi
+    echo \$2: $2
+    if [ -n "$2" ]; then
+        echo pass _n \$2
+    fi
+    if ( echo $2 | grep -q '[0-9]' ); then
+        echo pass grep on \$2
+    else
+        echo fail grep on \$2
+    fi
+    if [ -n "$2" ] && ( echo $2 | grep -q '[0-9]' ); then
+        lines="$2"
+    else
+        echo in else
+        lines="4"
+    fi
+    echo \$lines: $lines
+    cat $1|head -n$lines
+    echo "[...]"
+    cat $1|tail -n$lines
+    echo "(cat $1 head/tail $lines"
 }
 
 function show_help {
@@ -83,7 +115,7 @@ fi
 # Reset in case getopts has been used previously in the shell.
 OPTIND=1
 DISPLAYFILTER=""
-STREAMSLIST=""
+#STREAMSLIST=""
 KEYLOGFILE=""
 
 while getopts "h?r:dY:l:k:" opt; do
@@ -99,12 +131,13 @@ while getopts "h?r:dY:l:k:" opt; do
         echo \$DEBUG: $DEBUG
         non_int_q $non_int_d/0005-debug-set
         ;;
-    Y)  DISPLAYFILTER=$OPTARG
-        echo "gives: -Y $DISPLAYFILTER (\$DISPLAYFILTER); since \$OPTARG: $OPTARG"
-        ;;
-    l)  STREAMSLIST=$OPTARG
-        echo "gives: -l $STREAMSLIST (\$STREAMSLIST); since \$OPTARG: $OPTARG"
-        ;;
+    #TO_DO add code to use this? or remove?
+    #Y)  DISPLAYFILTER=$OPTARG
+    #    echo "gives: -Y $DISPLAYFILTER (\$DISPLAYFILTER); since \$OPTARG: $OPTARG"
+    #    ;;
+    #l)  STREAMSLIST=$OPTARG
+    #    echo "gives: -l $STREAMSLIST (\$STREAMSLIST); since \$OPTARG: $OPTARG"
+    #    ;;
     k)  KEYLOGFILE=$OPTARG
     echo "gives: -k $KEYLOGFILE (\$KEYLOGFILE); since \$OPTARG: $OPTARG"
         ;;
@@ -163,6 +196,10 @@ dump=${PCAP_FILE%*.pcap}
 echo \$ext: $ext
 echo \$dump: $dump
 
+# may not be needed:
+#TMP="$(mktemp -d "/tmp/$dump.$$.XXXXXXXX")"
+#ls -ld $TMP
+
 pcap_size=$(ls -lL --time-style=posix-long-iso $dump.$ext | awk '{print $5}')
 echo \$pcap_size: $pcap_size
 non_int_q $non_int_d/001-variables-echoed
@@ -190,107 +227,173 @@ non_int_q $non_int_d/002-past-size-limit
 #   Replacing it with:
 . shark2use
 
-# In case of interrupted runs (of say huge PCAPs):
-if [ ! -n "$STREAMSLIST" ]; then
-    if [ -e "${dump}_streams.ls-1" ]; then
-        STREAMSLIST=${dump}_streams.ls-1
-        ls -l ${dump}_streams.ls-1
-        echo "(ls -l ${dump}_streams.ls-1)"
-        ls -l $STREAMSLIST
-        echo "(ls -l $STREAMSLIST)"
-    else
-        echo "There is no \${dump}_streams.ls-1:"
-    fi
-fi
-non_int_q $non_int_d/003-past-streams-ls-1-check
-if [ ! -z "$DISPLAYFILTER" ]; then
-    echo \$DISPLAYFILTER: $DISPLAYFILTER
-    if [ -e "$STREAMSLIST" ] && [ -s "$STREAMSLIST" ]; then
-        echo "We'll be using the existing \$STREAMSLIST:" 
-        ls -l $STREAMSLIST
-        echo "(ls -l $STREAMSLIST)"
-    else
-        STREAMS=$($TSHARK -otls.keylog_file:$KEYLOGFILE -r "$dump.$ext" -Y "$DISPLAYFILTER" -T fields -e tcp.stream | sort -n | uniq)
-    fi 
-    if [ -e "${dump}_streams.ls-1" ] && [ -s "${dump}_streams.ls-1" ]; then
-        # backing up the list of stream numbers if previously made
-        cp -av ${dump}_streams.ls-1 ${dump}_streams.ls-1.PREV
-    fi
-    echo $STREAMS | tr ' ' '\012' > ${dump}_streams.ls-1
-    echo "############################################################"
-    echo "The list of stream numbers contained in the \$PCAP_FILE:"
-    echo "$PCAP_FILE is listed in:"
-    ls -l ${dump}_streams.ls-1
-    echo tail -2 ${dump}_streams.ls-1
-    tail -2 ${dump}_streams.ls-1
-    #echo "Hit Enter to continue!"
-    if [ -e "${dump}_streams.ls-1.PREV" ]; then
-        if ( diff ${dump}_streams.ls-1 ${dump}_streams.ls-1.PREV ); then
-            mv -v ${dump}_streams.ls-1.PREV ${dump}_streams.ls-1
-        fi
-    fi
-    echo "############################################################"
-
-    if [ ! -z "$STREAMSLIST" ]; then
-        echo \$STREAMSLIST: $STREAMSLIST
-        STREAMS=$(<$STREAMSLIST)
-    fi
-    non_int_q $non_int_d/008-end-if-displayfilter
+# previously here: if [ ! -n "$STREAMSLIST" ]; then...
+# However, recently the changes have gone global: tls.streams do not necessarily
+# correspond to tcp.streams, as used to be the case. So vars/files STREAMSLIST,
+# STREAMS need to be replaced per tcp.stream, per tls.stream and per
+# http2.streamid lists.
+# To be complete, there is also ip.stream, but I'm not sure it is needed so
+# I'll leave it out.
+# in "_fno_tcps_tlss_http2s_http2sid" fno is for frame.number, s is for streams
+if [ ! -e "${dump}_fno_tcps_tlss_http2s_http2sid.txt" ]; then 
+    touch ${dump}_fno_tcps_tlss_http2s_http2sid.txt.lock
+    $TSHARK -r $dump.$ext -T fields -e frame.number -e tcp.stream -e tls.stream -e http2.stream \
+        -e http2.streamid | grep '[[:print:]]' > ${dump}_fno_tcps_tlss_http2s_http2sid.txt
+    ls -l ${dump}_fno_tcps_tlss_http2s_http2sid.txt
+    rm -v ${dump}_fno_tcps_tlss_http2s_http2sid.txt.lock
 else
-    non_int_q $non_int_d/009-start-if-displayfilter-not
-    if [ -e "${dump}_streams.ls-1" ]; then
-        ls -l ${dump}_streams.ls-1
-        echo "(ls -l \${dump}_streams.ls-1)"
-    fi
-    if [ -e "$STREAMSLIST" ]; then
-        ls -l $STREAMSLIST
-        echo "(ls -l \$STREAMSLIST)"
-    fi
-    if [ -e "$STREAMSLIST" ] && [ -s "$STREAMSLIST" ]; then
-        echo "We'll be using the existing \$STREAMSLIST:" 
-        ls -l $STREAMSLIST
-        echo "(ls -l $STREAMSLIST)"
-        STREAMS=$(<$STREAMSLIST)
-    else
-        echo "\$TSHARK -otls.keylog_file:$KEYLOGFILE -r $dump.$ext -T fields -e tcp.stream | sort -n | uniq"
-        STREAMS=$($TSHARK -otls.keylog_file:$KEYLOGFILE -r "$dump.$ext" -T fields -e tcp.stream | sort -n | uniq)
-    fi
-
-    # $STREAMSLIST and ${dump}_streams.ls-1 are not always the same thing.
-    if [ ! -z "$STREAMSLIST" ]; then
-        echo \$STREAMSLIST: $STREAMSLIST
-        STREAMS=$(<$STREAMSLIST)
-        if [ -e "${dump}_streams.ls-1" ]; then
-            # backing up the list of stream numbers if previously made
-            cp -av ${dump}_streams.ls-1 ${dump}_streams.ls-1.PREV
-        fi
-    else
-        if [ -e "${dump}_streams.ls-1" ]; then
-            # backing up the list of stream numbers if previously made
-            cp -av ${dump}_streams.ls-1 ${dump}_streams.ls-1.PREV
-        fi
-        echo $STREAMS | tr ' ' '\012' > ${dump}_streams.ls-1
-        echo "############################################################"
-        echo "The list of stream numbers contained in the \$PCAP_FILE:"
-        echo "$PCAP_FILE is listed in:"
-        ls -l ${dump}_streams.ls-1
-        echo tail -2 ${dump}_streams.ls-1
-        tail -2 ${dump}_streams.ls-1
-        non_int_q $non_int_d/015-exist-streams-ls-1
-        echo "############################################################"
-        echo "In else 011"
-    fi
-    if [ -e "${dump}_streams.ls-1.PREV" ]; then
-        if ( diff ${dump}_streams.ls-1 ${dump}_streams.ls-1.PREV ); then
-            mv -v ${dump}_streams.ls-1.PREV ${dump}_streams.ls-1
-        fi
-    fi
-    non_int_q $non_int_d/020-end-if-displayfilter-not
+    while [ -e "${dump}_fno_tcps_tlss_http2s_http2sid.txt.lock" ]; do
+        ls -l ${dump}_fno_tcps_tlss_http2s_http2sid.txt.lock
+        echo sleep 5
+        sleep 5
+    done
 fi
 
+#NOTE 'sort -n -u' would say no stream when only one stream! Because it is
+#+ number 0. So 'sort -g -u'
+# If PCAP has no tcp.stream we will not work it
+#debug 3ln
+#awk 'BEGIN {FS="\t"; OFS="\t"} {print $2}' ${dump}_fno_tcps_tlss_http2s_http2sid.txt \
+#    | sort -g -u | grep '[[:print:]]'
+#read NOP
+awk 'BEGIN {FS="\t"; OFS="\t"} {print $2}' ${dump}_fno_tcps_tlss_http2s_http2sid.txt \
+    | sort -g -u | grep '[[:print:]]' > $dump-tcp.ls-1
+ls_head_tail $dump-tcp.ls-1 3
+echo \$dump_tcp_check: $dump_tcp_check
+dump_tcp_check=$(<$dump-tcp.ls-1)
+echo \$dump_tcp_check: $dump_tcp_check
+non_int_q $non_int_d/00201-dump-tcp-check
+if [ "X$dump_tcp_check" == "X" ]; then
+    echo "########################################################################"
+    echo "########################################################################"
+    echo "$dump.$ext has no tcp.stream's. sleep 5 and exit 0"
+    echo "########################################################################"
+    echo "########################################################################"
+    sleep 5
+    rm -v $dump-tcp.ls-1
+    exit 0
+fi
+
+# If PCAP has no tls.stream we will not work it
+#debug 3ln
+#awk 'BEGIN {FS="\t"; OFS="\t"} {print $3}' ${dump}_fno_tcps_tlss_http2s_http2sid.txt \
+#    | sort -g -u | grep '[[:print:]]'
+#read NOP
+awk 'BEGIN {FS="\t"; OFS="\t"} {print $3}' ${dump}_fno_tcps_tlss_http2s_http2sid.txt \
+    | sort -g -u | grep '[[:print:]]' > $dump-tls.ls-1
+ls_head_tail $dump-tls.ls-1 3
+echo \$dump_tls_check: $dump_tls_check
+dump_tls_check=$(<$dump-tls.ls-1)
+echo \$dump_tls_check: $dump_tls_check
+non_int_q $non_int_d/00202-dump-tls-check
+if [ "X$dump_tls_check" == "X" ]; then
+    echo "########################################################################"
+    echo "########################################################################"
+    echo "$dump.$ext has no tls.stream's. sleep 5 and exit 0"
+    echo "########################################################################"
+    echo "########################################################################"
+    sleep 5
+    rm -v $dump-tls.ls-1
+    exit 0
+fi
+
+# This sed inline produces exact copy of the ${dump}_fno_tcps_tlss_http2s_http2sid.txt it worked.
+# (tested on 100+ PCAPs with various kind of traffic)
+sed 's/\([1-9][0-9]\{,9\}\x09\([0-9]\{1,9\}\)\?\x09\)\([0-9]\{1,9\}\?\)\(,\3\)*\x09\([0-9]\{1,9\}\?\)\(\(,\5\)*\)\x09\(.*\)/\1\3\4\x09\5\6\x09\8/g' \
+    ${dump}_fno_tcps_tlss_http2s_http2sid.txt > ${dump}_fno_tcps_tlss_http2s_http2sid-SED.txt
+ls -l ${dump}_fno_tcps_tlss_http2s_http2sid-SED.txt
+if ( diff ${dump}_fno_tcps_tlss_http2s_http2sid.txt ${dump}_fno_tcps_tlss_http2s_http2sid-SED.txt ); then
+    rm -v ${dump}_fno_tcps_tlss_http2s_http2sid-SED.txt
+else
+    echo "We exit here, the method applied does NOT work right with:"
+    ls -l $dump.$ext
+    echo "Because the sed script (read the source did not work right."
+    echo "These files should be identical, and they differ:"
+    ls -l ${dump}_fno_tcps_tlss_http2s_http2sid.txt \
+        ${dump}_fno_tcps_tlss_http2s_http2sid-SED.txt
+    exit 1
+fi
+# sed offers only 9 backreferences. Need to work in stages.
+# The backreference \4 that matches  '\(,\3\)*' in the sed debugging can now be
+# freely left out.  Namely, if the sed script hadn't produced identical copy
+# above, that would mean there were other tls.stream numbers on that same line
+# in that field, and there are none.
+# The same holds for \6 matching '\(\(,\5\)*\)' and http2.stream numbers.
+#
+sed 's/\([1-9][0-9]\{,9\}\x09\([0-9]\{1,9\}\)\?\x09\)\([0-9]\{1,9\}\?\)\(,\3\)*\x09\([0-9]\{1,9\}\?\)\(\(,\5\)*\)\x09\(.*\)/\1\3\x09\5\x09\8/g' \
+    ${dump}_fno_tcps_tlss_http2s_http2sid.txt > ${dump}_fno_tcps_tlss_http2s_http2sid-SED-C.txt
+ls -l ${dump}_fno_tcps_tlss_http2s_http2sid-SED-C.txt # C is for clean
+non_int_q $non_int_d/00203-SED-C
+# In the tshark produced file, and reamins in the above results there is only
+# one tcp.stream per frame.number.
+# Now we've made that also only one tls.stream number is listed per frame number
+# And there's one http2.stream number per frame number.
+# I expect that http2.streamid's of any http2.stream come from same unique tls.stream.
+# So far I haven't seen the http2.stream would have any other number than 1.
+# I.e. no multiple http2 streams per tls stream.
+# Let's check if that is so with this PCAP:
+echo "awk '{print \$4}' \${dump}_fno_tcps_tlss_http2s_http2sid-SED-C.txt | sort -u | grep '[[:print:]]'"
+awk '{print $4}' ${dump}_fno_tcps_tlss_http2s_http2sid-SED-C.txt | sort -u | grep '[[:print:]]'
+awk 'BEGIN {FS="\t"; OFS="\t"} {print $4}' ${dump}_fno_tcps_tlss_http2s_http2sid-SED-C.txt \
+    | sort -g -u | grep '[[:print:]]' > $dump-http2.ls-1
+dump_http2_check=$(<$dump-http2.ls-1)
+echo \$dump_http2_check: $dump_http2_check
+echo X\$dump_http2_checkX: X${dump_http2_check}X
+if [ "X$dump_http2_check" == "X" ]; then
+    echo "There are no http2.streams in this PCAP"
+    non_int_q $non_int_d/00204-no-http2-streams
+else
+    if [ "$dump_http2_check" == "1" ]; then
+        echo "There's only http2.stream==1 ever from any number tls.stream."
+    else
+        echo "########################################################################"
+        echo "########################################################################"
+        echo "We exit here, the method applied does NOT work right with:"
+        ls -l $dump.$ext
+        echo "Because there are either more than 1 http2.stream's per some tls.streams."
+        echo "See:"
+        echo "awk '{print \$4}' \${dump}_fno_tcps_tlss_http2s_http2sid-SED-C.txt | sort -u | grep '[[:print:]]'"
+        awk '{print $4}' ${dump}_fno_tcps_tlss_http2s_http2sid-SED-C.txt | sort -u | grep '[[:print:]]'
+        echo "sleep 5 and exit 1"
+        echo "########################################################################"
+        echo "########################################################################"
+        sleep 5
+        exit 1
+    fi
+fi
+rm -v $dump-http2.ls-1
+
+awk 'BEGIN {FS="\t"; OFS="\t"} {print $1,$2,$3,$5}' ${dump}_fno_tcps_tlss_http2s_http2sid-SED-C.txt \
+    > ${dump}_fno_tcps_tlss_http2s_http2sid-SED-Cr.txt # Cr for [C]LEAN [r]eally
+# And we now have only 4 fields per any frame.number for the PCAP.
+ls -l ${dump}_fno_tcps_tlss_http2s_http2sid-SED-Cr.txt
+
+non_int_q $non_int_d/00207-SED-Cr
+# temporary: remove the below:
+#exit 0
+
+# We now from ...-SED-Cr.txt create lists $STREAMS-tcp and $STREAMS-tls.
+# Used to be what recently failed to apply. Big Tech were the first to introduce the said changes (see above):
+# STREAMS=$($TSHARK -otls.keylog_file:$KEYLOGFILE -r "$dump.$ext" -Y "$DISPLAYFILTER" -T fields -e tcp.stream | sort -n | uniq)
+STREAMS_tcpf="$dump-tcps.ls-1" # f is for file
+STREAMS_tlsf="$dump-tlss.ls-1"
+echo \$STREAMS_tcpf: $STREAMS_tcpf
+echo \$STREAMS_tlsf: $STREAMS_tlsf
+awk 'BEGIN {FS="\t"; OFS="\t"} {print $2}' ${dump}_fno_tcps_tlss_http2s_http2sid-SED-Cr.txt \
+    | sort -g -u | grep '[[:print:]]' > $STREAMS_tcpf
+awk 'BEGIN {FS="\t"; OFS="\t"} {print $3}' ${dump}_fno_tcps_tlss_http2s_http2sid-SED-Cr.txt \
+    | sort -g -u | grep '[[:print:]]' > $STREAMS_tlsf
+ls_head_tail $STREAMS_tcpf 3
+ls_head_tail $STREAMS_tlsf 3
+non_int_q $non_int_d/003-STREAMS-tcp-tls-files
+STREAMS_tcp=$(<$STREAMS_tcpf)
+echo \$STREAMS_tcp: $STREAMS_tcp
+STREAMS_tls=$(<$STREAMS_tlsf)
+echo \$STREAMS_tls: $STREAMS_tls
+non_int_q $non_int_d/00303-STREAMS-tcp-tls-vars
 > ${dump}_streams.ls-1_DONE #truncate, just in case
 # One loop to list existing streams if any
-for i in $STREAMS; do 
+for i in $STREAMS_tls; do 
     INDEX=`printf '%.3d' $i`
     for stream_file in ${dump}_s$INDEX.raw ${dump}_s$INDEX.raw.CLEAN ${dump}_s$INDEX.raw.FINAL ${dump}_s$INDEX.bin \
         ${dump}_s$INDEX.txt ${dump}_s$INDEX-tls.txt ; do
@@ -305,10 +408,7 @@ for i in $STREAMS; do
     done
     non_int_q $non_int_d/022-stream-file-raw
 done
-cat ${dump}_streams.ls-1_DONE
-echo "(cat ${dump}_streams.ls-1_DONE)"
-ls -l ${dump}_streams.ls-1_DONE
-echo "(ls -l ${dump}_streams.ls-1_DONE)"
+ls_head_tail ${dump}_streams.ls-1_DONE 3
 non_int_q $non_int_d/030-streams-ls-1-done
 echo "You can now set either:"
 echo ".skip_non-TLS_stream (type nt)"
@@ -317,7 +417,7 @@ echo ".skip_TLS_stream (type st)"
 non_int_q $non_int_d/040-befor-set-skip
 if [ "$skipping" == "nt" ]; then touch .skip_non-TLS_stream ; ls -l .skip_non-TLS_stream ; fi
 if [ "$skipping" == "st" ]; then touch .skip_TLS_stream ; ls -l .skip_TLS_stream ; fi
-for i in $STREAMS; do 
+for i in $STREAMS_tcp; do 
     # This can be adjusted manually. If really huge dump, I set %.4d, else %.3d is enough.
     INDEX=`printf '%.3d' $i`
     echo "Processing stream $INDEX ..."
@@ -346,6 +446,11 @@ for i in $STREAMS; do
             ls -l ${dump}_s$INDEX.txt
         fi
     fi
+done
+for i in $STREAMS_tls; do 
+    # This can be adjusted manually. If really huge dump, I set %.4d, else %.3d is enough.
+    INDEX=`printf '%.3d' $i`
+    echo "Processing stream $INDEX ..."
     if [ ! -e  ".skip_TLS_stream" ]; then
         if [ ! -e "${dump}_s${INDEX}-tls.raw" ] && [ ! -e "${dump}_s${INDEX}-tls.bin" ]; then
             echo "$TSHARK -otls.keylog_file:$KEYLOGFILE -r \"$dump.$ext\" -T fields -e data -qz follow,tls,raw,$i | grep -E '[[:print:]]' > ${dump}_s${INDEX}-tls.raw"
@@ -427,3 +532,4 @@ for i in $STREAMS; do
     fi
     non_int_q $non_int_d/099-befor-loop-item-end
 done
+# vim: set tabstop=4 expandtab:
